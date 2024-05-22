f9649bffd5a003d49c24f8d4da83c6a385e0cd71
Vinicius Charles <viniciuscharles3101@gmail.com>
2024-05-21 21:44:52 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,13 @@ public class Program {
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("Entre piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+				
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
